086c5f6e2135afcc9e07d1d6e47fe57c4f3e2ee8
Jenny Allen <jennifer.allen@yale.edu>
2014-07-09 14:34:24 -0400

    Final Commit

--- morethancoffee/config/environments/production.rb
+++ morethancoffee/config/environments/production.rb
@@ -80,4 +80,29 @@ Morethancoffee::Application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'mail.google.com', 
+   user_name:            'morethancoffeewebapp@gmail.com',
+   password:             'morethancoffee',
+   enable_starttls_auto: true,
+   authentication:       'plain',
+   enable_starttls_auto: true  }
+
+   # config/initializers/gravatar_image_tag.rb
+  GravatarImageTag.configure do |config|
+    config.default_image           = 'http://www.charlieproctor.com/img/id.jpg'   # Set this to use your own default gravatar image rather then serving up Gravatar's default image [ 'http://example.com/images/default_gravitar.jpg', :identicon, :monsterid, :wavatar, 404 ].
+    config.filetype                = 'jpg'   # Set this if you require a specific image file format ['gif', 'jpg' or 'png'].  Gravatar's default is png
+    config.include_size_attributes = true  # The height and width attributes of the generated img will be set to avoid page jitter as the gravatars load.  Set to false to leave these attributes off.
+    config.rating                  = nil   # Set this if you change the rating of the images that will be returned ['G', 'PG', 'R', 'X']. Gravatar's default is G
+    config.size                    = nil   # Set this to globally set the size of the gravatar image returned (1..512). Gravatar's default is 80
+    config.secure                  = false # Set this to true if you require secure images on your pages.
+  end
 end
