9087439bf947a89f52174b46b511d4d0449b13e4
Jenny Allen <jennifer.allen@yale.edu>
2014-07-09 14:29:06 -0400

    Hopefully removed the problems

--- morethancoffee/app/controllers/user_mailer_controller.rb
+++ morethancoffee/app/controllers/user_mailer_controller.rb
@@ -1,11 +1,2 @@
 class UserMailerController < ApplicationController
-	def create
-      puts params.to_s
-      @receiver = User.find(params[:user][:receiver_id])
-      puts "This is the receiver " + @receiver.id.to_s
-      @event = Event.find(params[:event_id])
-      UserMailer.contact_email(current_user, @receiver, @event).deliver
-      flash[:success] = "Email successfully sent!"
-      redirect_to user_url
-	end
 end
